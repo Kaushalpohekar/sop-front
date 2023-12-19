86e08e5806bcbadb8c0b5bb9e378bf471d0f3073
YatharthDeshpande <yatharthdeshpande724@gmail.com>
2023-12-19 17:57:20 +0530

    Layout

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,16 +4,26 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { OverviewComponent } from './overview/overview.component';
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     OverviewComponent,
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class DashboardModule { }
